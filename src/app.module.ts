682bd9c53c25934e152cf837639559cace9b4163
Talal <talalzafar84@gmail.com>
2025-02-14 14:14:09 +0500

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,7 @@ import { RamModule } from './ram/ram.module';
 import { GPUModule } from './gpu/gpu.module';
 import { ConditionModule } from './condition/condition.module';
 import { StorageModule } from './storage/storage.module';
-import { BlogsModule } from './brands copy/blogs.module';
+import { BlogsModule } from './blogs/blogs.module';
 
 @Module({
   imports: [
